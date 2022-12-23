618ccc2d29b5ba899af5966814c5161bf27d7d41
Shrinath Mishra <shrinath_mishra@outlook.com>
2022-12-23 11:05:20 +0530

    Add files via upload

---
+++ recursion.java
@@ -0,0 +1,16 @@
+public class Recursion {
+
+    public static void main(String[] args) {
+        int number = 5;
+        int sum = addNumbers(number);
+        System.out.println("Sum = " + sum);
+    }
+
+    public static int addNumbers(int num) {
+        if (num != 0)
+            return num + addNumbers(num - 1);
+        else
+            return num;
+    }
+}
+
