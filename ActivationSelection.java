84df405f31c06a9546e2ba413b5f126d47380694
Shrinath Mishra <shrinath_mishra@outlook.com>
2022-12-25 18:20:18 +0530

    Add files via upload

---
+++ ActivationSelection.java
@@ -0,0 +1,28 @@
+
+public class ActivationSelection {
+    
+    public static void printMaxActivities(int s[], int f[], int n) {
+        int i,j;
+        System.out.println("Following activities are selected");
+        i = 0;
+        System.out.println(i + " ");
+
+        for(j = 1; j<n; j++) {
+            if(s[j] >= f[i]) {
+                System.out.println(j + " ");
+                i = j;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        int s[] = {1,3,0,5,8,5};
+        int f[] = {2,4,6,7,9,9};
+        int n = s.length;
+
+        printMaxActivities(s, f, n);
+        
+    }
+}
